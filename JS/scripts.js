0c92a58c6d6f30ddcf6e49dd0d900383dfecfd13
geomio <geoff.millner22@gmail.com>
2021-02-22 12:02:50 -0800

    hid letter with css

---
+++ JS/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const person1Input = $("input#name").val();
+    const person2Input = $("input#title").val();
+    const daysInput= $("input#days").val();
+    const formalityInput = $("input#formality").val();
+
+    $("#name").text(person1Input);
+    $("#title").text(person2Input);
+    $("#days").text(daysInput);
+    $("#formality").text(formalityInput);
+    $("#letter").show();
+  
+    event.preventDefault();
+  });
+ });
